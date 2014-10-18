251d7a62d631ffb1291229bdbc27f762f81d692e
artbn <artbn@users.noreply.github.com>
2014-10-17 22:25:27 -0400

    simple steps

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-	var searchNumberPrev = 0
-	var searchNumberNext = 0
-	var author = ""
-	var id = ""
-	var link = "https://www.reddit.com/live/ta535s1hq2je/updates/"
 	$('.btn').click(function(){
 		var searchNumber = $(searchTB).val()
-        $('<p>').text(searchNumber).prependTo('.searchResult')
-		}
+        	console.log(searchNumber)
 })
